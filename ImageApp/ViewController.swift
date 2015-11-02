1521504cd6192c7c0d7088396a699724ccd7a347
Theodore Abshire <theodoreabshire@Macs-MacBook-Pro.local>
2015-11-02 12:52:26 -0800

    First commit

---
+++ ImageApp/ViewController.swift
@@ -0,0 +1,36 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+		
+		let status = PFObject(className: "Status")
+		status["text"] = "Status text"
+		status["location"] = "Seattle"
+		status["array_example"] = ["haha", "this is an array", "it has stuff in it"]
+		status.saveInBackgroundWithBlock()
+			{ (success, error) -> Void in
+				if success
+				{
+					print("Successfully saved to parse. Check parse console.")
+				}
+		}
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
